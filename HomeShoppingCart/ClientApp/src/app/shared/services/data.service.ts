842ff4ba1421abfb89c685cd980342f70bf952ea
Anoop Sasidharan <anoopas.as@gmail.com>
2021-08-20 13:15:06 -0400

    code for cart reserve

--- HomeShoppingCart/ClientApp/src/app/shared/services/data.service.ts
+++ HomeShoppingCart/ClientApp/src/app/shared/services/data.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ICart } from '../Models/icart';
 
@@ -8,15 +8,11 @@ import { ICart } from '../Models/icart';
 })
 export class DataService {
   _baseUrl: string;
-  //constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-  //  this._baseUrl = baseUrl;
-  //}
-  constructor(private http: HttpClient) {
-    //this._baseUrl = baseUrl;
+  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this._baseUrl = baseUrl;
   }
-
+  
   CreatNewCart(cart: ICart): Observable<any> {
-    return null;
-    //return this.http.post<ICart>(this._baseUrl + `api\Cart`, cart);
+    return this.http.post<ICart>(this._baseUrl + `api\Cart`, cart);
   }
 }
