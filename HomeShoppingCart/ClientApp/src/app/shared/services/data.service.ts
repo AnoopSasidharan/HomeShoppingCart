c1060d5ce684f1ef10fc4fd6d4160195fe355173
Anoop Sasidharan <anoopas.as@gmail.com>
2021-08-20 12:03:03 -0400

    adding call to create cart back1

--- HomeShoppingCart/ClientApp/src/app/shared/services/data.service.ts
+++ HomeShoppingCart/ClientApp/src/app/shared/services/data.service.ts
@@ -8,9 +8,10 @@ import { ICart } from '../Models/icart';
 })
 export class DataService {
 
-  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
+  constructor(private http: HttpClient) { }//, @Inject('BASE_URL') private baseUrl: string) { }
 
   CreatNewCart(cart: ICart): Observable<any> {
-    return this.http.post<ICart>(this.baseUrl + `Cart`, cart);
+    return null;
+    //return this.http.post<ICart>(this.baseUrl + `Cart`, cart);
   }
 }
---
+++ HomeShoppingCart/Models/CartBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeShoppingCart.Models
+{
+    public abstract class CartBase
+    {
+        public DateTime CreatedDate { get; set; }
+        public DateTime? CompletedDate { get; set; }
+    }
+}
